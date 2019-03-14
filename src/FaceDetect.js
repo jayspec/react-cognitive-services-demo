3285f8e2710d2df1cbad512c27ef28890ae2080d
Jason Specland <jaspecla@microsoft.com>
2019-03-14 00:51:11 -0400

    created face detection component

---
+++ src/FaceDetect.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import getFaceDetectionFromUrl from './faceDetectionService';
+
+function FaceDetect() {
+    const defaultImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/3/37/Dagestani_man_and_woman.jpg';
+    const [imageUrl, setImageUrl] = useState(defaultImageUrl);
+    const [faceDetectionJson, setFaceDetectionJson] = useState("");
+  
+    function updateFaceDetection() {
+        getFaceDetectionFromUrl(imageUrl)
+        .then(result => {
+            setFaceDetectionJson(result);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    return (
+      <div>
+        <form>
+            <input
+            value={imageUrl}
+            onChange={e => setImageUrl(e.target.value)}
+            placeholder="Image URL"
+            type="text"
+            name="imageUrl"
+            required />
+        <button type="button" onClick={() => updateFaceDetection()}>
+          Update Image
+        </button>
+        </form>
+        <div>
+            <img src={imageUrl} width="400" />
+        </div>
+        <div>
+            {faceDetectionJson}
+        </div>
+      </div>
+    );
+    }
+
+export default FaceDetect;
+
+
